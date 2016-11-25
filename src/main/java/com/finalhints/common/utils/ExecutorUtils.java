8a738650e4c14a727f50f550f476b641f0ea2ea1
Amit Bhoraniya <amitbhoraniya007@gmail.com>
2016-11-25 21:45:33 +0530

    adds executor utils code

--- src/main/java/com/finalhints/common/utils/ExecutorUtils.java
+++ src/main/java/com/finalhints/common/utils/ExecutorUtils.java
@@ -1,9 +1,32 @@
 package com.finalhints.common.utils;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 /**
  * @author amitbhoraniya
  *
  */
 public class ExecutorUtils {
 
+	public static String execute(String cmd) {
+		StringBuffer sb = new StringBuffer();
+		try {
+			Process process = Runtime.getRuntime().exec(cmd);
+			InputStream is = process.getInputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			String line = br.readLine();
+			while (line != null) {
+				sb.append(line);
+				sb.append(System.lineSeparator());
+				line = br.readLine();
+			}
+			process.waitFor();
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
+		return sb.toString();
+	}
 }
--- src/main/java/com/finalhints/common/utils/FileUtils.java
+++ src/main/java/com/finalhints/common/utils/FileUtils.java
@@ -1,8 +1,12 @@
 package com.finalhints.common.utils;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -31,12 +35,54 @@ public class FileUtils {
 		return null;
 	}
 
-	public static String readFileToString(File fileOrDir) {
-		return null;
+	public static String readFileToString(File file) {
+		if (!file.exists() || file.isDirectory())
+			return null;
+		StringBuffer sb = new StringBuffer();
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(file));
+			String line = br.readLine();
+			while (line != null) {
+				sb.append(line);
+				sb.append(System.lineSeparator());
+				line = br.readLine();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return sb.toString();
 	}
 
-	public static List<String> readLines(File fileOrDir) {
-		return null;
+	public static List<String> readLines(File file) {
+		if (!file.exists() || file.isDirectory())
+			return null;
+
+		List<String> lines = new ArrayList<String>();
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(file));
+			String line = br.readLine();
+			while (line != null) {
+				lines.add(line);
+				line = br.readLine();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return lines;
 	}
 
 	public static void writeToFile(InputStream source, File destination) {
@@ -52,10 +98,10 @@ public class FileUtils {
 	}
 
 	public static File getUserDir() {
-		return null;
+		return new File(System.getProperty("user.home"));
 	}
 
 	public static File getTempDir() {
-		return null;
+		return new File(System.getProperty("java.io.tmpdir"));
 	}
 }
