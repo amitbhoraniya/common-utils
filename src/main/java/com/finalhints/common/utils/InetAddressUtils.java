513d61fe2b91d7f13f10486f43ea841ace926873
Amit Bhoraniya <amitbhoraniya007@gmail.com>
2016-11-29 10:57:15 +0530

    adds get Localhost method

--- src/main/java/com/finalhints/common/utils/InetAddressUtils.java
+++ src/main/java/com/finalhints/common/utils/InetAddressUtils.java
@@ -1,9 +1,47 @@
 package com.finalhints.common.utils;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author amitbhoraniya
- *
  */
 public class InetAddressUtils {
 
+	public static Set<String> getAllLocalHostNames() {
+		Set<String> localNames = new HashSet<String>();
+		Enumeration<NetworkInterface> e = null;
+		try {
+			e = NetworkInterface.getNetworkInterfaces();
+		} catch (SocketException exception) {
+			throw new RuntimeException(exception);
+		}
+		while (e.hasMoreElements()) {
+			for (Enumeration<InetAddress> ee = e.nextElement().getInetAddresses(); ee
+					.hasMoreElements();) {
+				InetAddress ia = ee.nextElement();
+				if (ia != null) {
+					if (ia.getHostName() != null) {
+						localNames.add(ia.getCanonicalHostName());
+					}
+					if (ia.getHostAddress() != null) {
+						localNames.add(ia.getHostAddress());
+					}
+				}
+			}
+		}
+		final String localhost = "localhost";
+		if (!localNames.contains(localhost)) {
+			localNames.add(localhost);
+		}
+		final String localhostLocaldomain = "localhost.localdomain";
+		if (!localNames.contains(localhostLocaldomain)) {
+			localNames.add(localhostLocaldomain);
+		}
+		return localNames;
+	}
 }
